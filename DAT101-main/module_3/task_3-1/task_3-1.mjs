d453514e24adafc26e671d77290af1d8f0257ef4
VanitazCodes <cak332@gmail.com>
2024-10-28 14:40:50 +0100

    initial commit

--- DAT101-main/module_3/task_3-1/task_3-1.mjs
+++ DAT101-main/module_3/task_3-1/task_3-1.mjs
@@ -5,39 +5,83 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+printOut("--- Part 1 -------------------------------------------");
+
+let wakeUpTime = 6;
+
+if (wakeUpTime === 7) {
+  printOut("If I wake up at exactly 7 o'clock, I can take the bus to school.");
+} else if (wakeUpTime === 8) {
+  printOut("If I wake up at exactly 8 o'clock, I can take the train to school.");
+} else {
+  printOut("Otherwise, I have to take the car to school.");
 }
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let number = 2002;
+
+if (number > 0) {
+    printOut("Positive");
+  } else if (number < 0) {
+   printOut("Negative");
+  } else {
+   printOut("Zero");
+  }
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("--- Part 6 & 7 ----------------------------------------------------------------------------------------------");
+let imageSize = Math.floor(Math.random() * 8) + 1; // rANDOM Number Generator
+
+  printOut("Uploaded image size: " + imageSize + " MP");
+
+if (imageSize >= 6) {
+  printOut("Image is too large!");
+} else if (imageSize >= 4) {
+  printOut("Thank you!");
+} else {
+  printOut("The image is too tiny!");
+}
 printOut(newLine);
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+printOut("--- Part 8 & 9 ----------------------------------------------------------------------------------------------");
+const months = [
+  { name: "January", days: 31 },
+  { name: "February", days: 28 },
+  { name: "March", days: 31 },
+  { name: "April", days: 30 },
+  { name: "Mai", days: 31 },
+  { name: "June", days: 30 },
+  { name: "July", days: 31 },
+  { name: "August", days: 31 },
+  { name: "September", days: 30 },
+  { name: "October", days: 31 },
+  { name: "November", days: 30 },
+  { name: "December", days: 31 },
+]; //I corrected it to English as some months were not spelled quite right. :)
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const randomMonth = months[Math.floor(Math.random() * months.length)];
 
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+if (randomMonth.name.includes("r")) {
+  printOut("You must take vitamin D");
+} else {
+  printOut("You do not need to take vitamin D");
+  printOut(newLine);
+}
 
-/* Task 10*/
+printOut(`The month of ${randomMonth.name} has ${randomMonth.days} days.`);
+printOut(newLine);
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const currentMonthIndex = new Date().getMonth(); // 0 for January and then forward.
+const currentMonth = months[currentMonthIndex];
+
+if (currentMonth.name === "March" || currentMonth.name === "May") {
+  printOut("Gallery is closed for refurbishment. Will open soon!");
+} else if (currentMonth.name === "April") {
+  printOut("Temporary premises are open next door! :)");
+} else {
+  printOut("Gallery is open! Welcome!");
+}
 printOut(newLine);
